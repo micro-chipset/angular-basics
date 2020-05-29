2ddf438c53317cf5146b4e6ecede6a8eacd29fe8
micro-chipset <tyrnov.ivan@gmail.com>
2020-05-29 16:12:15 +0300

    add ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13]
+
+  objs = [
+    {title: 'Post 1', author: 'Ivan', comments: [
+        {name: 'Max', text: 'Lorem 1'},
+        {name: 'Max', text: 'Lorem 2'},
+        {name: 'Max', text: 'Lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Ivan', comments: [
+        {name: 'Max', text: 'Lorem 1'},
+        {name: 'Max', text: 'Lorem 2'},
+        {name: 'Max', text: 'Lorem 3'},
+      ]}
+  ]
 }
