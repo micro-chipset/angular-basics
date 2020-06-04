039b64830ef7650d408b54ad4f898f930f37d870
micro-chipset <tyrnov.ivan@gmail.com>
2020-06-04 13:37:21 +0300

    add pass props in directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -2,6 +2,7 @@ import {
   Directive,
   ElementRef,
   HostListener,
+  Input,
   Renderer2,
 } from '@angular/core';
 
@@ -9,6 +10,9 @@ import {
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'blue'
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string}
+
   constructor(private el: ElementRef, private r: Renderer2) {
   }
 
@@ -17,10 +21,16 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    this.r.setStyle(this.el.nativeElement, 'color', this.color)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+    this.r.setStyle(this.el.nativeElement, 'border', null)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 }
