3f283fac6a76248d27bbdfc731fa78b56ea45570
micro-chipset <tyrnov.ivan@gmail.com>
2020-06-04 13:10:02 +0300

    add HostListener in directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostListener,
   Renderer2,
 } from '@angular/core';
 
@@ -9,6 +10,17 @@ import {
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event);
+  }
+
+  @HostListener('mouseenter') onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null)
   }
 }
